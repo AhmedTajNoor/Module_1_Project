897f94f2315096c558dc5d0c07501df41681adf8
Ahmed Taj Noor <atnshafi@gmail.com>
2025-03-05 20:52:07 +0600

    Initial commit

---
+++ simpleElectricityBill.php
@@ -0,0 +1,15 @@
+<?php
+
+echo"Enter units consumed : ";
+
+$unit = (int)readline();
+
+if( $unit <= 100){
+    $bill = $unit * 5;
+}elseif( $unit <= 200){
+    $bill = 100 *5 + ($unit - 100) * 10;
+}else{
+    $bill = 100 *5 + 100*10 + ($unit - 200) * 15;
+}
+
+echo "Total bill : $$bill";
