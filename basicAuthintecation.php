dc43ee247d235f5779fc4a5239f21a69fdc19a62
Ahmed Taj Noor <atnshafi@gmail.com>
2025-03-07 18:03:21 +0600

    Update basicAuthintecation.php

--- basicAuthintecation.php
+++ basicAuthintecation.php
@@ -1,1 +1,20 @@
 <?php
+
+define("USERNAME","admin");
+define("PASSWORD","12345");
+
+echo"Enter Username: ";
+$Username = readline();
+
+echo"Enter Password: ";
+$Password = readline();
+
+if(empty($Username) || empty($Password)) {
+    echo "User name or Password is blank";
+}else{
+    if($Username === USERNAME && $Password === PASSWORD) {
+        echo "Login Successful !\n";
+    }else{
+        echo "Invalid Username or Password ! \n";
+    }
+}
